b13584a408691cd2016b5e7897ff9143b286f2c5
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2026-03-14 16:30:31 +0530

    Update prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,28 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-const adapter = new PrismaMariaDb({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
 export const prisma =
-  globalForPrisma.prisma ??
+  global.prisma ||
   new PrismaClient({
-    adapter,
-    log: ["error"],
+    log: ["query", "info", "warn", "error"],
   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
