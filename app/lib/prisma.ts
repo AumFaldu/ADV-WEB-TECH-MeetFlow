8a8390f7dd0a3529c9b036722f527311d84bef0e
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2026-03-15 13:13:54 +0530

    Update prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,14 +1,23 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+
 const prisma =
   global.prisma ||
   new PrismaClient({
+    adapter,
     log: ["query", "info", "warn", "error"],
-    engineType: 'library',
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
