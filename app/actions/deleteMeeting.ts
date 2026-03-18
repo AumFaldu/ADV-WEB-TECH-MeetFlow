30eaa7acacbc55e644671c39d1fcd8b789bf2566
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2026-03-18 15:40:33 +0530

    Update deleteMeeting.ts

--- app/actions/deleteMeeting.ts
+++ app/actions/deleteMeeting.ts
@@ -3,12 +3,34 @@
 import { prisma } from "@/app/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import cloudinary from "@/app/lib/cloudinary";
 import { requirePermission } from "../lib/auth";
 
 export async function deleteMeeting(meetingID: number, reason: string) {
-  const user = requirePermission("CANCEL_MEETING")
+
+  await requirePermission("CANCEL_MEETING");
+
   if (!meetingID) {
     throw new Error("Meeting ID is required");
+  }
+
+  const meeting = await prisma.meetings.findUnique({
+    where: { MeetingID: meetingID },
+    select: { DocumentPath: true },
+  });
+
+  if (meeting?.DocumentPath) {
+    try {
+      const parts = meeting.DocumentPath.split("/");
+      const fileName = parts[parts.length - 1];
+      const publicId = `meetflow/meetings/${fileName.split(".")[0]}`;
+
+      await cloudinary.uploader.destroy(publicId, {
+        resource_type: "raw",
+      });
+    } catch (err) {
+      console.log("Cloudinary delete failed", err);
+    }
   }
 
   await prisma.meetings.update({
