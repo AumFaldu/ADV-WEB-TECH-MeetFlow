44667ee7ca93536df5dc07bc6e49b3861bb746d9
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2026-03-21 10:55:39 +0530

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,32 +1,17 @@
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 import DashboardLayoutClient from "./DashboardLayoutClient";
+import {requireAuth} from "@/app/lib/auth"
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("authToken")?.value;
-
-  let role: "ADMIN" | "CONVENER" | "STAFF" = "STAFF";
-
-  if (token) {
-    try {
-      const decoded = jwt.verify(
-        token,
-        process.env.JWT_SECRET!
-      ) as any;
-
-      role = decoded.role;
-    } catch {
-      role = "STAFF";
-    }
-  }
+  const user = await requireAuth()
 
   return (
-    <DashboardLayoutClient role={role}>
+    <DashboardLayoutClient role={user.role}>
       {children}
     </DashboardLayoutClient>
   );
